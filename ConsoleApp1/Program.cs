fd4762a40cd6987606bdf1fc065e0c48199209e2
Jan Middelburg <51588796+JanM62@users.noreply.github.com>
2023-07-24 12:41:27 +0200

    Update message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Good morning Vietnam!");
+            Console.WriteLine("Good morning Pijnacker!");
         }
     }
 }
